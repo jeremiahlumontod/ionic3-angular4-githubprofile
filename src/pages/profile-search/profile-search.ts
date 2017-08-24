93726b247e652b5ef6c98a972be7880fbcb605b1
Jeremiah Lumontod <jeremiah.lumontod@gmail.com>
2017-08-25 04:09:27 +0800

    text and button for profile search

--- src/pages/profile-search/profile-search.ts
+++ src/pages/profile-search/profile-search.ts
@@ -14,7 +14,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'profile-search.html',
 })
 export class ProfileSearchPage {
-
+  username: string;
+  
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
